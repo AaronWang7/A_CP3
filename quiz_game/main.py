65707dd579a8ccf485dbd7a890eca1039cb35e42
AaronWang7 <aaron.wang@ucas-edu.net>
2025-08-15 11:24:43 -0600

    Updated the user answer system

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,17 +1,43 @@
 import csv 
 import random
 question = []
-while True:
+answer = []
+count = 0
 
+
+while True:
+    count = count +1
     def open_question():
         with open('quiz_game\question.csv', 'r') as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
+                answer.append({
+                    "Answer": row["answer"]
+                })
                 question.append({
-                    "Question": row["question"],
-                    "answer": row["answer"],
+                    "Question": row["question"]
 
                 })
+            print(question[count])
+            answer_question()
+    def answer_question():
+        print("Enter your answer")
+        user_input = input(":")
+        answer_checker = answer[count]
+        if "{" + "'Answer'" + ":" +" " +"'" + " " + user_input + "'" + "}" == str(answer_checker):
+            print("You are right!","The answer is: ",user_input)
+        else:
+            print(f"You are wrong! The right aswer is {answer[count]}")
+            print(answer)
+            print(answer_checker)
+        
+        
+        
+        
+        if "{'Answer': ' A'}" == str(answer[0]):
+            print("Yes")
+
+        else:
+            print("No")
 
     open_question()
-    print(question[1])
