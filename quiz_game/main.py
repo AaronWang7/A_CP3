0c531dea1fd3b12b7eacb2def28216c5af94c6ad
AaronWang7 <aaron.wang@ucas-edu.net>
2025-08-19 11:48:15 -0600

    updated score system

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -6,12 +6,17 @@ count = 0
 user_score = 0
 score = 0
 breaking = False
+time = 0
+best_score = None
+
+reset = False
 
 
 while True:
     if breaking == False: 
-        count = random.randint(0,6)
+        count = random.randint(0,48)
         def open_question():
+            global best_score
             with open('quiz_game\question.csv', 'r') as file:
                 csv_reader = csv.DictReader(file)
                 for row in csv_reader:
@@ -23,28 +28,35 @@ while True:
 
                     })
                 print(question[count])
+
+
                 answer_question()
         def answer_question():
+            global time
             global user_score
             print("Enter your answer")
             user_input = input("press Q to exit:")
+            
             answer_checker = answer[count]
             if "{" + "'Answer'" + ":" +" " +"'" + user_input + "'" + "}" == str(answer_checker):
                 print("You are right!","The answer is: ",user_input)
-                user_score = user_score + 1
+                print(time)
+                reset = True
+                user_score = user_score + 100
                 print(f"Your score is: {user_score}")
+
+
             elif user_input == "Q":
                 print("saving...")
                 saving_score()
                 global breaking
                 breaking = True
 
-
             else:
                 print(f"You are wrong! The right aswer is {answer[count]}")
-                print(answer)
                 print(answer_checker)
             
+                
             
             
             
@@ -55,7 +67,8 @@ while True:
                 print("No")
         def saving_score():
             with open("quiz_game/score.csv", "a") as f:
-                f.write(f"Score: {user_score}")
+                f.write("Score\n")
+                f.write(user_score)
     else:
         break
     
