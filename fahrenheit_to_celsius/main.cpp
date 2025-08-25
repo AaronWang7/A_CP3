f62c790e090967c7c9d58979d8df0ca0ae538389
AaronWang7 <aaron.wang@ucas-edu.net>
2025-08-25 11:25:13 -0600

    out put degress Fahrenheit and cellsius

--- fahrenheit_to_celsius/main.cpp
+++ fahrenheit_to_celsius/main.cpp
@@ -7,6 +7,6 @@ int main(){
     float user_input = 0;
     cin >> user_input;
     float celsius = (user_input - 32) * 5/9;
-    cout << user_input << " Fahrenheit is " << celsius<< " Cellsius"<<endl;
+    cout << user_input << " Degrees Fahrenheit is " << celsius<< " Degrees Celsius"<<endl;
     return 0;
 }
