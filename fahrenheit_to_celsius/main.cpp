7839ef6134aa752213e12d6c4197a246911b28e4
AaronWang7 <aaron.wang@ucas-edu.net>
2025-08-25 11:20:50 -0600

    Fahrenheit to Celisus fished

---
+++ fahrenheit_to_celsius/main.cpp
@@ -0,0 +1,12 @@
+// AW Fahrenheit to Celsius
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<< "Enter in Fahrenheit:";
+    float user_input = 0;
+    cin >> user_input;
+    float celsius = (user_input - 32) * 5/9;
+    cout << user_input << " Fahrenheit is " << celsius<< " Cellsius"<<endl;
+    return 0;
+}
