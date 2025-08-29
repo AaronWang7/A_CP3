3140481f662d0a53be1b57be7a65c9ef88a1582e
AaronWang7 <aaron.wang@ucas-edu.net>
2025-08-29 11:49:14 -0600

    updated user login

--- Notes/arrays.cpp
+++ Notes/arrays.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <limits>           
 using namespace std;
-string fam[10] = {"Cira", "Eric", "Alex", "Maya", "Liam", "Sophie", "Noah", "Emma", "Lucas", "Olivia"};
-string sibs[8] = {"Alex", "Maya", "Liam", "Sophie", "Noah", "Emma", "Lucas", "Olivia"};
-
+# include <array>
+//string fam[10] = {"Cira", "Eric", "Alex", "Maya", "Liam", "Sophie", "Noah", "Emma", "Lucas", "Olivia"};
+//string sibs[8] = {"Alex", "Maya", "Liam", "Sophie", "Noah", "Emma", "Lucas", "Olivia"};
+int nums[50] = {5,4,1,3,8,2,7,6,0,9,22,11,33,55,44,66,88,77,99,10,32,12,14,16,18,15,13,17,19,20,34,36,38,35,37,39,40,42,41,43,45,47,46,48,49,50};
+/*
 int search(string list[],type_t len, string item){
     for(int i = 0; i < len, i++){
         if (list[i] == item){
@@ -15,8 +17,10 @@ int search(string list[],type_t len, string item){
         }
 }
     return 0;
-}
+*/
+
 int main(){
+    /*
     //if (fam == sibs){
         //cout << "Broken\n";
     //}
@@ -34,12 +38,54 @@ int main(){
    // structured bindings
    auto [q,w,e,r,t,y,u,i,o,p] = fam;
    cout << e << endl;
-   
+   */
+  
+   // rows and columns
+   //multi-dimensional array
+    //int matrix[3][3] = {1,2,3,},
+    //{4,5,6},
+    //{7,8,9};
+    int len = size(nums);
+    for (int j = 0; j < len; j++){
+        for (int i = 1; i < (len - 1) ; i++){     
+            cout << nums[i] << endl;
+            if(nums[i] < nums[i - 1]){
+                //swap(nums[i] nums[i + 1]);
+                    //cout << nums;
+                int swap = nums[i]; 
+                nums[i] = nums[i-1];
+                nums[i-1] = swap;
 
-    int matrix[3][3] = {1,2,3,},
-    {4,5,6},
-    {7,8,9}};
+            }
+        }
 
+    }
 
-    return 0;
+    for (int i = 0; i < len; i++){
+        cout << nums[i] << ",";
 }
+    return 0;
+
+}
+/*
+How are for loops constructed in C++: 
+When do you need to use curly brackets in C++:
+How do you compare items in arrays:
+How do you use an array as an argument in a function:
+What is type_t:
+Why would we use type_t:
+How do you search an array:
+How do you sort an array:
+How do you make a multi-dimensional array: 
+*/
+
+
+
+
+
+
+
+
+
+
+
--- user_login/main.cpp
+++ user_login/main.cpp
@@ -9,7 +9,18 @@ int main(){
     string admin_login[2] = {"kol999","slsj918"};
     cout << "Enter in your user name:";
     cin >> user_name;
-    string user_enter = user_login->find(user_name);
+    // check if user is in user_login array
+    int len_user = size(user_login);
+    int len_admin = size(admin_login);
+    for (int i = 0; i < len_user; i++){
+        if (user_name == user_login[i]){
+            cout << "Welcome back " << user_login[i] <<endl;
+        } else if (user_name == admin_login[i]){
+            cout << "Welcome admin " << admin_login[i] << endl;
+            {
+
+            }
+        }
 
     return 0;
     
