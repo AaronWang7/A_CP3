c3b6d87e2e2eab8fda79fef77d1bbdfead5e229e
AaronWang7 <aaron.wang@ucas-edu.net>
2025-08-29 23:32:01 -0600

    Update main.cpp

--- user_login/main.cpp
+++ user_login/main.cpp
@@ -1,27 +1,38 @@
-// Aw User login
-
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
     string user_name = "";
-    string user_login[5] = {"Tom","Aaron","Jack767","apple989","pie981"};
-    string admin_login[2] = {"kol999","slsj918"};
-    cout << "Enter in your user name:";
-    cin >> user_name;
-    // check if user is in user_login array
-    int len_user = size(user_login);
-    int len_admin = size(admin_login);
-    for (int i = 0; i < len_user; i++){
-        if (user_name == user_login[i]){
-            cout << "Welcome back " << user_login[i] <<endl;
-        } else if (user_name == admin_login[i]){
-            cout << "Welcome admin " << admin_login[i] << endl;
-            {
+    string user_login[5] = {"Tom", "Aaron", "Jack767", "apple989", "pie981"};
+    string admin_login[2] = {"kol999", "slsj918"};
 
-            }
+    cout << "Enter in your user name: ";
+    cin >> user_name;
+
+    bool isAdmin = false;
+    bool isUser = false;
+
+    for (int i = 0; i < 2; i++) {
+        if (user_name == admin_login[i]) {
+            isAdmin = true;
+            break;
         }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (user_name == user_login[i]) {
+            isUser = true;
+            break;
+        }
+    }
+
+    if (isAdmin) {
+        cout << "Hello Admin " << user_name << endl;
+    } else if (isUser) {
+        cout << "Welcome back " << user_name << endl;
+    } else {
+        cout << "Let's get you signed up " << user_name << endl;
+    }
 
     return 0;
-    
 }
