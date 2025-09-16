51e9172a1f312dafd0595c54e7f3625b02fac5f8
AaronWang7 <aaron.wang@ucas-edu.net>
2025-09-16 11:48:48 -0600

    Sreuctures notes

---
+++ Notes/structures.cpp
@@ -0,0 +1,120 @@
+// Aw Strucutres Notes
+
+# include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Date {
+    short year = 1900;
+    short month = 1;
+    short day = 1;
+
+};
+struct person {
+    string name;
+    Date ssidYear;
+    string email;
+
+    bool == (person aaron){
+        if (name == aaron.name &&
+            ssidDate.year == aaron,releaseDate.month
+        ){
+            return true;
+            cout << "Equal";
+        
+    }else{
+        return false;
+        cout << "not equal";
+    }
+}
+};
+
+int mian(){
+    vector <person> aarons;
+    
+    //Movie movie;//Unclear 
+    //movie.title = "Temrinator";
+    person aaron = {"Aaron",123,"Email"};
+    aarons.push_back({"Aaron",123});
+    arrons.push_back({"Aaron 2",234});
+
+    person aaron1 = {"Aaron",123,"Email"};
+    person aaron2 = {"Aaron2",234,"Emails"};
+
+    auto [name, ssid, email] {aaron};
+
+    for (auto aaron: aarons){
+
+        cout << name << endl;
+        cout << ssidYear.year << endl;
+
+    }
+    cout << aaron1.equals(aaron2) << endl;
+    //cout << "Movie and Title: " << movie << endl; 
+    
+    if (movies.name == movie2.name){
+        cout << "Equal";
+    
+    }else{
+        cout << "Not Equal";
+    }
+    
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+What are structures?
+How do you build a structure?
+What can be held in a structure?
+How do you access the information in a structure?
+How do you overload an operator
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
