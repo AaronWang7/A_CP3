5c0278f64f0fde308260acb8fe7ba148cb6cb98b
AaronWang7 <aaron.wang@ucas-edu.net>
2025-09-15 21:51:11 -0600

    Update main.cpp

--- dynamic array/main.cpp
+++ dynamic array/main.cpp
@@ -1,81 +1,50 @@
-// deynamic array AW
-
 #include <iostream>
-
-#include <memory>
-
 #include <string>
 
 using namespace std;
 
+int main() {
+    int cap = 5; 
+    int entries = 0; 
 
-int main(){
-    int cap = 5;
-    string user_enter[] = {"red", "blue", "green", "yellow", "purple"};
-    int entries = 0;
-    while(true){
-        cout << "Enter something you could think of(Books, Movies, Shows, Colors, Comics, etc.) :";
-        cin >> user_enter[entries];
-        entries++;
-        cout << "You entered: " << user_enter[entries-1] << endl;
-        if (entries == cap){
-            cap = cap + 5;
-            cout << cap << endl;
-            string* temp = new string[cap];
-            for (int i=0; i < entries-1; i++){
-                temp[i] = user_enter[i];
-
-            }
-        
-        }
     
+    string* user_enter = new string[cap];
 
+    cout << "Enter items (Books, Movies, Shows, Colors, Comics, etc.). Type 'done' to finish.\n";
+
+    while (true) {
+        cout << "Enter something: ";
+        string input;
+        getline(cin, input);
+
+        if (input == "done") { 
+            break;
+        }
+
+        
+        if (entries == cap) {
+            cap += 5; 
+            string* temp = new string[cap];
+
+            
+            for (int i = 0; i < entries; i++) {
+                temp[i] = user_enter[i];
+            }
+
+            delete[] user_enter; 
+            user_enter = temp;  
+        }
+
+        user_enter[entries] = input; 
+        entries++;
+    }
+    
+    cout << "\nYou entered:\n";
+    for (int i = 0; i < entries; i++) {
+        cout << i + 1 << ". " << user_enter[i] << endl;
+    }
+
+    
+    delete[] user_enter; 
     return 0;
-
 }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-Write a program that asks the user to enter books, movies, shows, colors, comics, etc. until they can't think of anymore. 
-
-Start your array with only 5 spaces. Continue to increase the size of the array as your user hits the array cap. 
-
-THEN, print out each of the inputs they gave you!
-
-KEY REMINDERS:
-Remember you have to set your variable types
-You have to get the standard library to create your inputs and outputs 
-Make sure your arrows are pointing the right way for the input/output stream. 
-You have to set the length of the array when you create it
-
-*/
